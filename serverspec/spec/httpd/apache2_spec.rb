f1780be774a295427e17e0fae516f094d0c12eed
akihiro-o2k <akihiro.otuka@ntt.com>
2022-07-11 08:27:17 +0000

    new file: ansible/vars/common.yml(common valiables definition file)

--- serverspec/spec/httpd/apache2_spec.rb
+++ serverspec/spec/httpd/apache2_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
 
-# apache2パッケージがインストールされている事。
-describe package('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
-end
-
-# apache2が起動時に有効化設定されており、動作している事。
-describe service('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
+describe 'apache2.4パッケージがインストールされている事。'
+  describe package('apache2'), :if => os[:family] == 'ubuntu' do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
 
 # 80番ポートがListenになっている事。
@@ -23,7 +20,7 @@ describe file('/etc/apache2/apache2.conf') do
 end
 
 # phpモジュールのインストール状態確認
-describe command('apachectl -M |grep php5_module') do
-  it { should return_stdout /php5_module/ }
+describe command('apachectl -M |grep libapache2-mod-php8.0') do
+  it { should return_stdout /php8.0_module/ }
 end
 
