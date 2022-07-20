47d87a920756a498c530726dd1e4921e262fe739
akihiro-o2k <akihiro.otuka@ntt.com>
2022-07-20 02:49:29 +0000

    add:serverspec->mariadb specg st

--- serverspec/spec/db/mariadb_spec.rb
+++ serverspec/spec/db/mariadb_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
-describe package('mariadb-server') do
-  it { should be_installed }
+describe 'mariadbパッケージがインストールされている事。' do
+  describe package('mariadb'), :if => os[:family] == 'ubuntu' do
+    it { should be_installed }
+  end
 end
-
-describe service('mariadb') do
+describe service('mariadb'), :if => os[:family] == 'ubuntu' do
   it { should be_enabled   }
   it { should be_running   }
+end
+=begin
+describe 'MySQL config parameters' do
+  context mysql_config('innodb-buffer-pool-size') do
+    its(:value) { should > 10000 }
+  end
+
+  context mysql_config('socket') do
+    its(:value) { should eq '/tmp/mysql.sock' }
+  end
 end
 
 ### ポイント: ポートが特定のIPアドレスでListenしているか確認（2.0からの新機能） ###
@@ -29,3 +40,4 @@ end
 describe command("mysqladmin -u#{db_user} -p#{db_password} variables |grep character_set_server") do
   it { should return_stdout /utf8/ }
 end
+=end
