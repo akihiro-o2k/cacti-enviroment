87a947ad9bf0e9983391de33453702c12aab8051
akihiro-o2k <akihiro.otuka@ntt.com>
2022-07-25 08:36:26 +0000

    [serverspec]add: mariadb_spec->installd version check & db_user grant db check

--- serverspec/spec/db/mariadb_spec.rb
+++ serverspec/spec/db/mariadb_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
-describe 'mariadbパッケージがインストールされている事。' do
-  describe package('mariadb'), :if => os[:family] == 'ubuntu' do
+describe 'mariadb-serverパッケージがインストールされている事。' do
+  describe package('mariadb-server'), :if => os[:family] == 'ubuntu' do
     it { should be_installed }
+  end
+  describe command("apt list --installed | grep mariadb-server-#{COMMON['mariadb_version']}") do
+    # mariadb-server-10.8/
+    its(:stdout)  { should match /^mariadb-server-10.8\// }
   end
 end
 describe 'systemd関連:mariadbの起動と再起動時の動作を確認' do
@@ -26,22 +30,35 @@ describe 'MySQL config parameters' do
     its(:value) { should eq '/tmp/mysql.sock' }
   end
 end
-
-### ポイント: ポートが特定のIPアドレスでListenしているか確認（2.0からの新機能） ###
-
-
-db_user = "visualize"
-db_password = "Visualize00330033"
-
-describe command("mysqlshow -u#{db_user} -p#{db_password}") do
-  it { should return_stdout /zabbix/ }
+=end
+=begin
+describe command("mysqlshow -uroot -p#{COMMON['mysql_root_password']} -h#{COMMON['bind-address']}") do
+  it { should return_stdout /cacti/ }
+end
+=end
+describe "rootユーザーのmysqlshow実行結果:配列のDBが表示される事" do
+  array = %w(cacti information_schema mysql performance_schema sys)
+  describe command("mysqlshow -uroot -p#{COMMON['mysql_root_password']} -h#{COMMON['bind-address']}") do
+    array.each do |param|
+      its(:stdout)  { should match /#{param}/ }
+    end
+  end
+end
+describe "#{COMMON['db_user_name']}ユーザーのmysqlshow実行結果:配列のDBが表示される事" do
+  describe command("mysqlshow -u#{COMMON['db_user_name']} -p#{COMMON['db_user_password']} -h#{COMMON['bind-address']}") do
+     %w(cacti information_schema).each do |param|
+      its(:stdout)  { should match /#{param}/ }
+    end
+  end
 end
 
-describe command("mysqlshow -u#{db_user} -p#{db_password} cacti") do
+
+=begin
+describe command("mysql -u#{COMMON['db_user_name']} -p#{COMMON['db_user_password']} cacti") do
   it { should return_stdout /hosts/ }
 end
 
-describe command("mysqladmin -u#{db_user} -p#{db_password} variables |grep character_set_server") do
-  it { should return_stdout /utf8/ }
+describe command("mysqladmin -u#{COMMON['db_user_name']} -p#{COMMON['db_user_password']} variables |grep character_set_server") do
+  it { should return_stdout /utf8mb4/ }
 end
 =end
