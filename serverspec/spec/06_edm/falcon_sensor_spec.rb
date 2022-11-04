bb7f8d1287ebb3f57f37089495a76b8ee255e2d1
akihiro-o2k <akihiro.otuka@ntt.com>
2022-11-04 16:17:30 +0900

    [serverspec] add falcon_sensor spec

---
+++ serverspec/spec/06_edm/falcon_sensor_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+if ENV['ENVIROMENT'] == 'production'
+  describe "[1]:falcon-sensorパッケージのインストール状態確認::" do
+    describe package('falcon-sensor'), :if => os[:family] == 'ubuntu' do
+      it { should be_installed }
+    end
+  end
+  describe "[2]falcon-sensorの動作確認::" do
+    describe process("falcon-sensor") do
+      it { should be_running }
+    end
+  end
+  describe "[3]falcon-sensorのlogrotate設定の確認::" do
+    describe file('/etc/logrotate.d/falcon-sensor') do
+      it { should be_directory }
+      it { should be_mode 4644 }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      its(:content) { should match /rotate 24/ }
+      its(:content) { should match /monthly/ }
+    end
+  end
+end
