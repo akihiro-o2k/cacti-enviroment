0db2b67ba00e02c9c7d32ad3f8819d61b4240f39
akihiro-o2k <akihiro.otuka@ntt.com>
2023-09-06 17:51:12 +0900

    Work:2023-09-06 production env feed back & refacters commit

--- serverspec/spec/06_edm/falcon_sensor_spec.rb
+++ serverspec/spec/06_edm/falcon_sensor_spec.rb
@@ -13,8 +13,8 @@ if ENV['ENVIROMENT'] == 'production'
   end
   describe "[3]falcon-sensorのlogrotate設定の確認::" do
     describe file('/etc/logrotate.d/falcon-sensor') do
-      it { should be_directory }
-      it { should be_mode 4644 }
+      it { should be_file }
+      it { should be_mode 644 }
       it { should be_owned_by 'root' }
       it { should be_grouped_into 'root' }
       its(:content) { should match /rotate 24/ }
