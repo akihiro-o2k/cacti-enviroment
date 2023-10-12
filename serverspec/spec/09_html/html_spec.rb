d35e1bb04d309249366c369d6e43539963631feb
akihiro-o2k <akihiro.otuka@ntt.com>
2023-10-12 14:01:51 +0900

    fix serverspec and ansible for html(tm-cacti.tgz)

--- serverspec/spec/09_html/html_spec.rb
+++ serverspec/spec/09_html/html_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 describe "[1]directory存在確認::" do
   describe file('/var/www/html/tm-cacti') do
     it { should be_directory }
-    it { should be_mode 755 }
+    it { should be_mode 644 }
     it { should be_owned_by 'www-data' }
     it { should be_grouped_into 'www-data' }
   end
 end
 describe "[2]html存在確認::" do
   describe file('/var/www/html/tm-cacti/index.html') do
-    it { should be_directory }
+    it { should be_file }
     it { should be_mode 644 }
     it { should be_owned_by 'www-data' }
     it { should be_grouped_into 'www-data' }
   end
   describe file('/var/www/html/tm-cacti/office.html') do
-    it { should be_directory }
+    it { should be_file }
     it { should be_mode 644 }
     it { should be_owned_by 'www-data' }
     it { should be_grouped_into 'www-data' }
